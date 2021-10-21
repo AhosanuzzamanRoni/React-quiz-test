49eecd93b6140ece557268f2f1abcba93a7419cd
AhosanuzzamanRoni <86812185+AhosanuzzamanRoni@users.noreply.github.com>
2021-10-21 12:19:40 +0600

    commit message

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import logo from "../assets/images/logo-bg.png";
+import logo from "../assets/images/A.Roni.png";
 import classes from "../styles/Nav.module.css";
 import Account from "./Account";
 
@@ -9,8 +9,8 @@ export default function Nav() {
       <ul>
         <li>
           <Link to="/" className={classes.brand}>
-            <img src={logo} alt="Learn with Sumit Logo" />
-            <h3>Learn with Sumit</h3>
+            <img src={logo} alt="Learn with Roni Logo" />
+            <h3>Learn with Roni</h3>
           </Link>
         </li>
       </ul>
